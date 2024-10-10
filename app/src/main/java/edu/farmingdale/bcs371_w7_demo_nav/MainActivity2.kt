3aa83279cf0218a73c10f1fd314061736e10192d
OsmanyARdx <osmarguet@gmail.com>
2024-10-10 14:49:04 -0400

    Complete: ToDo 5

--- app/src/main/java/edu/farmingdale/bcs371_w7_demo_nav/MainActivity2.kt
+++ app/src/main/java/edu/farmingdale/bcs371_w7_demo_nav/MainActivity2.kt
@@ -52,7 +52,7 @@ class MainActivity2 : ComponentActivity() {
 @Composable
 fun BasicOperations(name: String, modifier: Modifier = Modifier) {
     val  context = LocalContext.current
-
+    var checkBox = false
     Column {
         Spacer(modifier = Modifier.padding(50.dp))
         Button( onClick = {
@@ -97,8 +97,8 @@ fun BasicOperations(name: String, modifier: Modifier = Modifier) {
 
         // ToDo 5: This switch is not working fix it
         Switch(
-            checked = true,
-            onCheckedChange = {  },
+            checked = checkBox,
+            onCheckedChange = {  checkBox = it },
             modifier = Modifier.padding(10.dp),
         )
         // ToDo 6: when the switch is off, disable the buttons
